72df4482c7fd494c1cc1045a27e01f4c0ba25bb8
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-10-03 02:10:40 +0100

    clean up permutation code

--- specs/src/permutation.rs
+++ specs/src/permutation.rs
@@ -1,8 +1,5 @@
 use crypto::{PublicParameters, RootsOfUnity};
-//
-// TODO: currently the permutation is not doing anything
-// TODO: first need to test the methods being used
-//
+
 /// There are some structures which need to be permuted.
 /// We implement this trait on such structures
 /// This allows us to leave the cryptography untouched
@@ -17,22 +14,20 @@ pub trait Permutable {
 impl Permutable for RootsOfUnity {
     type PermutedType = RootsOfUnity;
     fn permute(self) -> Self::PermutedType {
-        self
-        // let permutation = bit_reversal_permutation(&self.inner);
-        // RootsOfUnity {
-        //     inner: permutation,
-        //     inverse_domain_size: self.inverse_domain_size,
-        // }
+        let permutation = bit_reversal_permutation(&self.inner);
+        RootsOfUnity {
+            inner: permutation,
+            inverse_domain_size: self.inverse_domain_size,
+        }
     }
 }
 
 impl Permutable for PublicParameters {
     type PermutedType = PublicParameters;
-    fn permute(self) -> Self::PermutedType {
-        self
+    fn permute(mut self) -> Self::PermutedType {
         // Permute the lagrange vectors in the commitment key
-        // self.commit_key.inner = bit_reversal_permutation(&self.commit_key.inner);
-        // self
+        self.commit_key.inner = bit_reversal_permutation(&self.commit_key.inner);
+        self
     }
 }
 
@@ -41,9 +36,12 @@ fn is_power_of_two(value: usize) -> bool {
     value.is_power_of_two()
 }
 
+// This method mimics what the python method does
+// but it has hidden behavior if you are not familiar with
+// c how c string formats work
 // Note: We do not care about the speed of this function at all.
 // Since it is only ran on startup.
-fn reverse_bits(n: usize, order: usize) -> usize {
+fn reverse_bits(num: usize, order: usize) -> usize {
     // This assert avoids the edge case of order == 0
     // when getting the bit size of order - 1
     //
@@ -51,14 +49,43 @@ fn reverse_bits(n: usize, order: usize) -> usize {
     // panicking here is fine
     assert!(is_power_of_two(order));
 
-    let n_as_bits = to_bits(n as u32);
-    let num_bits_needed = to_bits((order - 1) as u32).len();
+    let num: usize = num
+        .try_into()
+        .expect("cannot convert num to a 32 bit integer");
+    let order: usize = order
+        .try_into()
+        .expect("cannot convert order to a 32 bit integer");
 
-    let mut n_as_bits = n_as_bits[0..num_bits_needed].to_vec();
-    // Reverses the order of bit vector
-    n_as_bits.reverse();
+    // First find out how many bits of number we need to consider
+    let num_bits_needed = num_bits_needed((order - 1) as u32);
 
-    from_bits(n_as_bits) as usize
+    // b - Tells the formatter to format the number as a binary string
+    // 0 - Tells the formatter to pad the binary string with zeroes
+    // 0$ - This is a placeholder for the width of the resulting binary string.
+    // the 0 tells the formatter to get the value frm the first argument
+    // to the macro. In this case, num_bits_needed
+    //
+    // Note: If the width is less than the number of bits needed
+    // to represent the number, then it will print out all of its bits
+    let num_as_binary_string = format!("{num:00$b}", num_bits_needed);
+
+    // Reverse the order of the binary string
+    let rev_num_as_binary_string = rev_str(num_as_binary_string);
+
+    // Interpret the binary string as number
+    usize::from_str_radix(&rev_num_as_binary_string, 2).unwrap()
+}
+// Returns how many bits are needed to represent a number
+// Look at the bit representation of 32 bit number and it is 32
+// minus the number of leading zeroes
+fn num_bits_needed(number: u32) -> usize {
+    (u32::BITS - number.leading_zeros()) as usize
+}
+
+fn rev_str(v: String) -> String {
+    let mut chars: Vec<_> = v.chars().collect();
+    chars.reverse();
+    chars.into_iter().collect()
 }
 
 // Return a copy with bit-reversed permutation. This operation is idempotent.
@@ -68,39 +95,6 @@ pub fn bit_reversal_permutation<T: Clone>(vector: &[T]) -> Vec<T> {
         .map(|i| reverse_bits(i, vector.len()))
         .map(|p_i| vector[p_i].clone())
         .collect()
-}
-
-// Converts a number to bits and truncates any prepending zeroes
-fn to_bits(x: u32) -> Vec<bool> {
-    let num_bits = u32::BITS;
-    let bits: Vec<_> = (1..=num_bits).map(|i| x & (1u32 << i) != 0).collect();
-
-    // Truncate prepending zero bits
-    let mut index_first_bit_set = None;
-    for (index, bit) in bits.iter().enumerate() {
-        if *bit == true {
-            index_first_bit_set = Some(index);
-            break;
-        }
-    }
-
-    match index_first_bit_set {
-        Some(index) => bits[index..].to_vec(),
-        None => {
-            // This means that no bts were set, ie its 0
-            vec![false]
-        }
-    }
-}
-fn from_bits(bit_vec: Vec<bool>) -> u32 {
-    let mut result = 0u32;
-
-    let base = 2u32;
-    for (index, bit) in bit_vec.into_iter().enumerate() {
-        result += base.pow(index as u32) * (bit as u32);
-    }
-
-    result
 }
 
 #[test]
